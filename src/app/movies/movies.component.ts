1f8ea1f00581339adae8f688bd65bcd5351e7166
jenkrisu <jenni.unkuri@gmail.com>
2018-05-17 10:50:15 +0300

    Add loading spinner and favicon

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/observable';
 
-import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
@@ -35,10 +35,13 @@ export class MoviesComponent implements OnInit {
     .debounceTime(500)
     .filter(query => query.length > 0)
     .distinctUntilChanged()
+    .do(() => this.emptyResults())
+    .do(() => this.loading = true)
+    // SwitchMap: "flattening operator", cancels previous inner observable and subscribes to the new one
     .switchMap(query => this.searchService.search('movie', query))
+    .do(() => this.loading = false )
     .subscribe(
       response => {
-        this.error = '';
         this.total_results = response.body.total_results;
         this.results = response.body.results;
         console.log(response.body.results);
@@ -50,19 +53,29 @@ export class MoviesComponent implements OnInit {
   }
 
   getPage(page: number) {
+    this.error = '';
+    this.results = [];
+    this.loading = true;
     this.page = page;
     this.searchService
     .search('movie', this.query.value, page)
     .subscribe(
       response => {
-        this.error = '';
+        this.loading = false;
         this.total_results = response.body.total_results;
         this.results = response.body.results;
         console.log(response.body.results);
       },
       error => {
+        this.loading = false;
         this.error = error;
-      }
-    );
+      });
   }
+
+  emptyResults() {
+      this.error = '';
+      this.total_results = 0;
+      this.results = [];
+  }
+
 }
